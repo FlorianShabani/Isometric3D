b60eca04a75ab2e2daa87c277a1507cc888d7abd
Florian Shabani <86021222+Procalypse@users.noreply.github.com>
2021-08-01 04:42:38 +0200

    Add files via upload

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,134 +2,146 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.List;
 
-import calculations.Calc;
-import entities.Block3D;
+import entities3D.Block3D;
+import entities3D.Entity3D;
 import setup.Manager;
 import setup.Window;
 
 public class Main implements Manager {
 
-	public static final int Width = 500, Height = 500;
-
-	public static final int aX = 45, aY = -45;
-
+	long time = System.nanoTime();
 	int count = 0;
 
-	Block3D bl = new Block3D(0, 0, 0, aX, aY);
-	
-	int sx = 100, sy = 0, sz = -350;
+	public static int Width = 1200, Height = 500;
 
-	ArrayList<Block3D> b = new ArrayList<Block3D>();
+	public static int ix = Width / 2, iy = 250, iz = 550;
 
-	private BufferedImage grid = new BufferedImage(Width, Height,
-			BufferedImage.TYPE_4BYTE_ABGR);
+	List<Entity3D> blocks = new ArrayList<Entity3D>();
+	Entity3D ent = new Entity3D(0, Height , 0, 600, 100, 10000, ix, iy, iz, 230, 100, 20);
+	Entity3D ent2 = new Entity3D(0, Height - 500, 0, 600, 100, 10000, ix, iy, iz, 230, 100, 20);
 
 	public Main() {
-		drawGrid();
-		/*for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				b.add(new Block3D(i * 80 + sx, 0, j * 80 + sz, aX, aY));
-			}
-		}*/
-		bl.output("block");
+
+		blocks.add(ent);
+		blocks.add(ent2);
+		for (int i = 10; i >= 0; i--) {
+			blocks.add(new Block3D(-99, Height - 100, i * 1000, ix, iy, iz));
+			blocks.add(new Block3D(600, Height - 100, i * 1000, ix, iy, iz));
+		}
 	}
 
 	@Override
 	public void tick() {
-
+		
 	}
 
-	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.white);
 		g.fillRect(0, 0, Width, Height);
-		g.drawImage(grid, 0, 0, null);
-		for (Block3D d : b) {
-			d.draw(g);
+		for (Entity3D e : blocks) {
+			e.draw(g);
 		}
 	}
 
-	private void drawGrid() {
-		Graphics2D g = grid.createGraphics();
-		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-
-		rh.put(RenderingHints.KEY_RENDERING,
-				RenderingHints.VALUE_RENDER_QUALITY);
-
-		((Graphics2D) g).setRenderingHints(rh);
-		g.setColor(new Color(70, 255, 0, 75));
-		for (int i = 0; i < 1000; i += 40) {
-			int[] c = Calc.getD(0, 0, i - 400, aX, aY);
-			int[] c1 = Calc.getD(1000, 0, i - 400, aX, aY);
-			g.drawLine(c[0], c[1], c1[0], c1[1]);
+	@Override
+	public void keyPressed(int k) {
+		for (Entity3D e : blocks) {
+			e.updateGZ(ix, iy, iz);
+			e.updateGX(ix, iy, iz);
 		}
-		for (int i = 0; i < 1000; i += 40) {
-			int[] c = Calc.getD(i, 0, -400, aX, aY);
-			int[] c1 = Calc.getD(i, 0, 400, aX, aY);
-			g.drawLine(c[0], c[1], c1[0], c1[1]);
+		switch (k) {
+		case 87:
+			for (Entity3D e : blocks) {
+				e.update(0, 0, 10);
+			}
+			break;
+		case 83:
+			for (Entity3D e : blocks) {
+				e.update(0, 0, -10);
+			}
+			break;
+		case 65:
+			for (Entity3D e : blocks) {
+				e.update(-10, 0, 0);
+			}
+			break;
+		case 68:
+			for (Entity3D e : blocks) {
+				e.update(10, 0, 0);
+			}
+			break;
+		case 16:
+			for (Entity3D e : blocks) {
+				e.update(0, 10, 0);
+			}
+			break;
+		case 32:
+			for (Entity3D e : blocks) {
+				e.update(0, -10, 0);
+			}
+			break;
 		}
-		g.dispose();
+	}
+
+	@Override
+	public void keyReleased(int k) {
+
 	}
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Window window = new Window(Width, Height, 60, "3D", new Main());
+		Window wind = new Window(Width, Height, 60, 60, "Cool3D", new Main());
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-
-	}
-
-	@Override
-	public void keyPressed(int e) {
-
-	}
-
-	@Override
-	public void keyReleased(int e) {
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void keyTyped(int e) {
+		// TODO Auto-generated method stub
 
 	}
+
 }
